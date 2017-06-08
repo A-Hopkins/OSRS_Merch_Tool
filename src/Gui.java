9779f07a8bc6928ebe33e6ac76ce81b65c9404ec
Alex Hopkins <hopkinsalex@knights.ucf.edu>
2017-06-08 03:34:17 -0400

    Setting up GUI, added Gson support to add jsons to the table and added the textFields to add items flipped to the table.
    Created saveAll method. Incomplete
    Created reload method. Incomplete
    Created save method. Incomplete
    
    Created AbstractOSBuddyItemsTable, AbstractCalculationTable, AbstractEntryTable, and EntryInstance classes. Incomplete

--- src/Gui.java
+++ src/Gui.java
@@ -1,11 +1,27 @@
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import entry.EntryInstance;
+import entry.table.AbstractCalculationTable;
+import entry.table.AbstractEntryTable;
+import item.osbuddy.table.AbstractOSBuddyItemsTable;
+
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.AbstractTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.DateFormat;
 import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by Alex on 6/7/2017.
@@ -19,12 +35,21 @@ public class Gui extends JFrame {
 	private final JTextField sellingTextField;
 	private final JTextField amountTextField;
 	private final JTextField outputTextField;
+	private final JTextField logEntryTextField;
+	private final JTextField amountBoughtTextField;
+	private final JTextField soldForTextField;
+	private final JTextField amountSoldTextField;
+
+	private ArrayList<EntryInstance> entries;
+	private AbstractEntryTable entryTable;
+	private AbstractCalculationTable calculationTable;
+	private AbstractOSBuddyItemsTable itemTable;
 
 	/**
 	 *  Builds the UI of the Application and handles most of the processes.
 	 *
 	 */
-	Gui() {
+	Gui() throws IOException {
 
 		/*
 		 * General Application settings like size and title
@@ -60,6 +85,18 @@ public class Gui extends JFrame {
 		tabbedPane.addTab("Logs and Calculations", null, tablePanel, null);
 
 		tablePanel.setLayout(null);
+
+		/*
+		 * Loads data from a .json file into an ArrayList instanced by EntryInstance
+		 */
+		Gson gson = new Gson();
+		ArrayList<EntryInstance> _entries = gson.fromJson(Files.newBufferedReader(Paths.get("data","items.json")), new TypeToken<List<EntryInstance>>() {
+		}.getType());
+
+		/*
+		 * Set our global ArrayList to the local one loaded from the .json file.
+		 */
+		this.entries = _entries;
 
 		/*
 		 * Instance of a horizontal separator on the UI
@@ -143,7 +180,7 @@ public class Gui extends JFrame {
 		 * With Key listener to only include digits
 		 */
 		sellingTextField = new JTextField();
-		sellingTextField.setBounds(50, 331, 132, 20);
+		sellingTextField.setBounds(60, 331, 132, 20);
 		sellingTextField.setColumns(10);
 
 		sellingTextField.addKeyListener(new KeyAdapter() {
@@ -243,20 +280,17 @@ public class Gui extends JFrame {
 
 		/*
 		 * Instance of JButton Clear button
+		 *  Action listener for clearing text in buyingTextField, sellingTextField, amountTextField, and outputTextField
 		 */
 		JButton clearButton = new JButton("Clear");
 		clearButton.setFont(new Font("Tahoma", Font.BOLD, 13));
 		clearButton.setBounds(198, 423, 160, 22);
 
-		clearButton.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				buyingTextField.setText("");
-				sellingTextField.setText("");
-				amountTextField.setText("");
-				outputTextField.setText("");
-			}
+		clearButton.addActionListener(e -> {
+			buyingTextField.setText("");
+			sellingTextField.setText("");
+			amountTextField.setText("");
+			outputTextField.setText("");
 		});
 
 		tablePanel.add(clearButton);
@@ -264,11 +298,161 @@ public class Gui extends JFrame {
 		/*
 		 * Instance of JLabel Potential profit made
 		 */
-		JLabel potentialProfitMadeLabel = new JLabel("Potential Profit Made");
+		JLabel potentialProfitMadeLabel = new JLabel("Potential Profit");
 		potentialProfitMadeLabel.setFont(new Font("Tahoma", Font.BOLD, 13));
 		potentialProfitMadeLabel.setBounds(211, 304, 143, 14);
 
 		tablePanel.add(potentialProfitMadeLabel);
+
+		/*
+		 * Instance of JLabel Item Name
+		 */
+		JLabel itemNameLabel = new JLabel("Item Name");
+		itemNameLabel.setFont(new Font("Tahoma", Font.BOLD, 13));
+		itemNameLabel.setBounds(456, 251, 105, 14);
+
+		tablePanel.add(itemNameLabel);
+
+		/*
+		 * Instnace of JTextField for log Entry Text field
+		 */
+		logEntryTextField = new JTextField();
+		logEntryTextField.setBounds(466, 271, 277, 20);
+		logEntryTextField.setColumns(10);
+
+		tablePanel.add(logEntryTextField);
+
+		/*
+		 * Instance of JLabel Bought For
+		 */
+		JLabel boughtItemLabel = new JLabel("Bought For");
+		boughtItemLabel.setFont(new Font("Tahoma", Font.BOLD, 13));
+		boughtItemLabel.setBounds(456, 301, 92, 20);
+
+		tablePanel.add(boughtItemLabel);
+
+		/*
+		 * Instance of textField for amount bought
+		 * With key listener that consumes non digit characters
+		 */
+		amountBoughtTextField = new JTextField();
+		amountBoughtTextField.setBounds(466, 321, 277, 20);
+		amountBoughtTextField.setColumns(10);
+
+		amountBoughtTextField.addKeyListener(new KeyAdapter() {
+
+			@Override
+			public void keyTyped(KeyEvent e) {
+				char c = e.getKeyChar();
+
+				if (!Character.isDigit(c)) {
+					e.consume();
+				}
+			}
+		});
+
+		tablePanel.add(amountBoughtTextField);
+
+		/*
+		 * Instance of JLabel Sold For
+		 */
+		JLabel soldForLabel = new JLabel("Sold For");
+		soldForLabel.setFont(new Font("Tahoma", Font.BOLD, 13));
+		soldForLabel.setBounds(456, 351, 105, 14);
+
+		tablePanel.add(soldForLabel);
+
+		/*
+		 * Instance of JTextField soldForTextField
+		 * With action listener that consumes non digit input
+		 */
+		soldForTextField = new JTextField();
+		soldForTextField.setBounds(466, 371, 277, 20);
+		soldForTextField.setColumns(10);
+
+		soldForTextField.addKeyListener(new KeyAdapter() {
+
+			@Override
+			public void keyTyped(KeyEvent e) {
+				char c = e.getKeyChar();
+
+				if (!Character.isDigit(c)) {
+					e.consume();
+				}
+			}
+		});
+
+		tablePanel.add(soldForTextField);
+
+		/*
+		 * Instance of JLabel amountSoldLabel
+		 */
+		JLabel amountSoldLabel = new JLabel("Amount Sold");
+		amountSoldLabel.setFont(new Font("Tahoma", Font.BOLD, 13));
+		amountSoldLabel.setBounds(456, 401, 277, 14);
+
+		tablePanel.add(amountSoldLabel);
+
+		/*
+		 * Instance of amountSoldTextField
+		 * With key listener to consume non digit input
+		 */
+		amountSoldTextField = new JTextField();
+		amountSoldTextField.setBounds(466, 421, 277, 20);
+		amountSoldTextField.setColumns(10);
+
+		amountSoldTextField.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				char c = e.getKeyChar();
+
+				if (!Character.isDigit(c)) {
+					e.consume();
+				}
+			}
+		});
+
+		tablePanel.add(amountSoldTextField);
+
+		/*
+		 * JButton to insert item flipped data/information
+		 * Uses DateFormat to track dates
+		 * With actionlistener to add to table
+		 */
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Date date = new Date();
+
+		JButton tableInsertButton = new JButton("Insert to Table");
+		tableInsertButton.setFont(new Font("Tahoma", Font.BOLD, 13));
+		tableInsertButton.setBounds(552, 447, 130, 22);
+
+		tableInsertButton.addActionListener(arg0 -> {
+			entries.add(new EntryInstance(dateFormat.format(date), logEntryLabel.getText(),
+					Integer.parseInt(amountBoughtTextField.getText()),
+					soldForTextField.getText().equals("") ? 1 : Integer.parseInt(soldForTextField.getText()),
+					Integer.parseInt(amountSoldTextField.getText())));
+
+			Gui.this.saveAll();
+
+			logEntryLabel.setText("");
+			amountBoughtTextField.setText("");
+			soldForTextField.setText("");
+			amountSoldTextField.setText("");
+		});
+	}
+
+	public void saveAll() {
+		this.save(this);
+		this.reload(this);
+		entryTable.fireTableDataChanged();
+		calculationTable.fireTableDataChanged();
+	}
+
+	private void reload(Gui gui) {
+
+	}
+
+	private void save(Gui gui) {
 
 	}
 }
